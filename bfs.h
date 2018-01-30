9e65a9bd842b0ffe063bce17b4bea769a6defff2
dylan-gale <dylan.gale@utd.edu>
2018-01-29 22:21:59 -0500

    created a class file for bst

---
+++ bfs.h
@@ -0,0 +1,12 @@
+#ifndef BFS_H
+#define BFS_H
+#include <iostream>
+using namespace std;
+
+class bfs
+{
+	public:
+		bfs(string str){ cout << "bfs " << str << '\n';}
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include "bfs.h"
+#include <fstream>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	cout << "entered " << argc << "args:\n";
-	for(int i = 0; i < argc ; i++)
-		cout << argv[i] << '\n';
+	string txt = argv[1];
+       	ifstream file(txt, ios::in);
+	
+	string puzzle;
+	if(file)getline(file, puzzle);file.close();
+	bfs test(puzzle);
+	
+		
+
 	return 0;
 }
