7d2ed6c32a5db74a0de6bc2bb70d279275e9cab8
dylan-gale <dylan.gale@utd.edu>
2018-01-30 06:00:09 -0500

    added hash function for hashing visited nodes later

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include "bfs.h"
 #include <fstream>
+#include <cmath>
 using namespace std;
+
+//hash function that will treat the string of tiles as a ternary number
+//and convert it to binary for the sake of hashing already visited nodes.
+int hash(string tiles)
+{
+	int num = 0;
+	for( int i =0; i < sizeof(tiles);i++)
+	{
+		if(tiles[i] == 'B') num += 1 * pow(3,i);
+		else if(tiles[i] == 'W') num += 2 * pow(3,i);	
+	}
+	return num;
+}
+
+
 
 int main(int argc, char *argv[])
 {
