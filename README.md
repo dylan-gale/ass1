52b1e97008d33621eb2327ccb224aae16895b3b7
Dylan Gale <dylan.gale@utd.edu>
2018-01-31 03:23:24 -0600

    figured out the hashing situation so it won't segfault

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ass1
 assignment 1 artificial intelligence
+
+I spent an unreasonable amount of time on a hash function for a table to 
+keep record of already visited nodes. I saw immediately I could hash these 
+sets of tiles as ternary values, however initializing a worst case table
+of a proper size for this(six white(2), six black(1)), it continually segfaulted.
+Needed to move on though and decided to use this anyway and to initialize my hash
+tables later on to be of size 
--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@ using namespace std;
 
 //hash function that will treat the string of tiles as a ternary number
 //and convert it to binary for the sake of hashing already visited nodes.
-int hash(string tiles)
+int Hash(string tiles)
 {
 	int num = 0;
-	for( int i =0; i < sizeof(tiles);i++)
+	for( int i =0; i < tiles.length() ;i++)
 	{
+		cout << i << endl;
 		if(tiles[i] == 'B') num += 1 * pow(3,i);
 		else if(tiles[i] == 'W') num += 2 * pow(3,i);	
 	}
@@ -26,9 +27,21 @@ int main(int argc, char *argv[])
 	
 	string puzzle;
 	if(file)getline(file, puzzle);file.close();
-	bfs test(puzzle);
-	
-		
-
+	//bfs test(puzzle);
+	string big = "xBBBBBBWWWWWW";
+	//YES
+	//YES
+	//YES
+	//WE CAN USE THIS HASH FUNCTION STILL BY SIMPLY CHECKING IF THE VALUE
+	//HASHED TO STORES ZERO OR NOT, INSTEAD OF STORING THE DUMB FUCKING 
+	//STIRNGS THERE GOD DAMNIT I AM SUCH A FUCKING IDIOT.
+	int table[Hash(big)] = {0};	
+	cout << pow(3, big.length()) - Hash(big) << endl;
+	int tsize = pow(3, big.length());
+	int tabl2[tsize] = {0};
+	//ALRIGHT SO WHEN YOU SET THIS UP, TAKE THE INPUT STRING, INITIALIZE THE
+	//VISITED ARRAY TO BE AN INT ARRAY OF SIZE POW(3,INIT.LENGTH()) AND WE 
+	//GOOD
+	//
 	return 0;
 }
